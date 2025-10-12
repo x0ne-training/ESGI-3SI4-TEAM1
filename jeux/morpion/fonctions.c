9d9faf99ea0a95c186edd5a29ba1586d8422d870
lucie <lmanarin@myges.fr>
2025-10-12 17:24:08 +0200

    add: fonction de vérification de la victoire

--- jeux/morpion/fonctions.c
+++ jeux/morpion/fonctions.c
@@ -11,13 +11,13 @@ void initialiserGrille() {
     }
 }
 
-void AfficherGrille(){
+void afficherGrille() {
     printf("\n");
     printf("  1   2   3\n");
-    for (int i = 0; i < 3; i++){
+    for (int i = 0; i < 3; i++) {
         printf("%d ", i + 1);
-        for (int j = 0; j < 3; j++){
-            if grille[i][j] == 'X') {
+        for (int j = 0; j < 3; j++) {
+            if (grille[i][j] == 'X') {
                 printf("%sX%s", ROUGE, RESET);
             } else if (grille[i][j] == 'O') {
                 printf("%sO%s", BLEU, RESET);
@@ -29,4 +29,30 @@ void AfficherGrille(){
         printf("\n");
         if (i < 2) printf("  --+---+--\n");
     }
+}
+
+int verifierVictoire() {
+    int victoire = PAS_VICTOIRE;
+
+    for (int i = 0; i < 3; i++) {
+        if (grille[i][0] != ' ' && grille[i][0] == grille[i][1] && grille[i][1] == grille[i][2]) {
+            victoire = VICTOIRE;
+        }
+    }
+
+    for (int j = 0; j < 3; j++) {
+        if (grille[0][j] != ' ' && grille[0][j] == grille[1][j] && grille[1][j] == grille[2][j]) {
+            victoire = VICTOIRE;
+        }
+    }
+
+    if (grille[0][0] != ' ' && grille[0][0] == grille[1][1] && grille[1][1] == grille[2][2]) {
+        victoire = VICTOIRE;
+    }
+
+    if (grille[0][2] != ' ' && grille[0][2] == grille[1][1] && grille[1][1] == grille[2][0]) {
+        victoire = VICTOIRE;
+    }
+
+    return victoire;
 }
