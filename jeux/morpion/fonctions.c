1f3fbdc065263004e47ca9de522bc80f16e97e64
lucie <lmanarin@myges.fr>
2025-10-12 17:11:32 +0200

    add: fonction d'initilisation de la grille

--- jeux/morpion/fonctions.c
+++ jeux/morpion/fonctions.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 #include "morpion.h"
+
+char grille[3][3];
+
+void initialiserGrille() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            grille[i][j] = ' ';
+        }
+    }
+}
