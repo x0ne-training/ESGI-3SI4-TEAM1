3cbd3161895612f4c07b577eb71cf3c416903532
lucie <lmanarin@myges.fr>
2025-10-12 17:07:05 +0200

    add: inclusion des en-têtes

--- jeux/morpion/fonctions.c
+++ jeux/morpion/fonctions.c
@@ -1,3 +1,2 @@
-//
-// Created by luciehp on 12/10/2025.
-//
+#include <stdio.h>
+#include "morpion.h"
--- jeux/morpion/morpion.h
+++ jeux/morpion/morpion.h
@@ -1,8 +1,4 @@
-//
-// Created by luciehp on 12/10/2025.
-//
-
 #ifndef JEUX_MORPION_H
 #define JEUX_MORPION_H
 
-#endif //JEUX_MORPION_H
+#endif
