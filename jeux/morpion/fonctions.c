4c35ef55a57bcdac8952278d8c6ea7045a010e99
lucie <lmanarin@myges.fr>
2025-10-12 17:16:32 +0200

    add: fonction d'affichage de la grille

--- jeux/morpion/fonctions.c
+++ jeux/morpion/fonctions.c
@@ -10,3 +10,23 @@ void initialiserGrille() {
         }
     }
 }
+
+void AfficherGrille(){
+    printf("\n");
+    printf("  1   2   3\n");
+    for (int i = 0; i < 3; i++){
+        printf("%d ", i + 1);
+        for (int j = 0; j < 3; j++){
+            if grille[i][j] == 'X') {
+                printf("%sX%s", ROUGE, RESET);
+            } else if (grille[i][j] == 'O') {
+                printf("%sO%s", BLEU, RESET);
+            } else {
+                printf(" ");
+            }
+            if (j < 2) printf(" | ");
+        }
+        printf("\n");
+        if (i < 2) printf("  --+---+--\n");
+    }
+}
