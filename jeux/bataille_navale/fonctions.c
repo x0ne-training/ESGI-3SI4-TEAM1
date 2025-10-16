ee08fb03aaeab2db8be3fba2b9144493e672c3ad
lucie <lmanarin@myges.fr>
2025-10-16 19:57:55 +0200

    add: constantes

---
+++ jeux/bataille_navale/fonctions.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "fonctions.h"
+
+
+const BateauInfo BATEAUX[NB_BATEAUX] = {
+        {5, "Porte-avions"},
+        {4, "Croiseur"},
+        {3, "Contre-torp"},
+        {3, "Sous-marin"},
+        {2, "Torpilleur"}
+};
