bb668b7e7c2e9d717fefb17ec1e26259350d0820
lucie <lmanarin@myges.fr>
2025-10-16 19:14:41 +0200

    add: structure des différentes actions

---
+++ jeux/bataille_navale/fonctions.h
@@ -0,0 +1,19 @@
+#ifndef FONCTIONS_H
+#define FONCTIONS_H
+
+#include <stdbool.h>
+
+#define TAILLE 10
+#define NB_BATEAUX 5
+
+typedef enum {
+    VIDE = 0,
+    BATEAU,
+    TOUCHÉ,
+    MANQUÉ,
+    COULÉ
+} Case;
+
+#endif
+
+};
