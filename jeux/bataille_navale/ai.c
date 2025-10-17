563ff9106cc4c230725fd398f0db338e2cab0df6
lucie <lmanarin@myges.fr>
2025-10-17 14:34:57 +0200

    add: robot

--- jeux/bataille_navale/ai.c
+++ jeux/bataille_navale/ai.c
@@ -2,3 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include "ai.h"
+
+
+static Point stack_targets[TAILLE*TAILLE];
+static int stack_top = 0;
+
+void ai_init(void) {
+    srand((unsigned)time(NULL) ^ 0xC0FFEE);
+    stack_top = 0;
+}
+
