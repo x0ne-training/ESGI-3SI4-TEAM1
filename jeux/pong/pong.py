1ce011b9f4ff9e7b1c864d4bb5648e48ad7351b9
“rayane” <r.germondchibane@myskolae.com>
2025-10-21 09:21:59 +0200

    création de la variable score

--- jeux/pong/pong.py
+++ jeux/pong/pong.py
@@ -6,3 +6,7 @@ fenetre.title("Pong by Rayane")
 fenetre.bgcolor("black")
 fenetre.setup(width=800, height=600)
 fenetre.tracer(0)  # désactive les mises à jour automatiques
+
+# Score
+score_gauche = 0
+score_droite = 0
