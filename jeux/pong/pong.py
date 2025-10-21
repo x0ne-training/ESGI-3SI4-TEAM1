59ef6ea8cd6ec76df7308fc0cee5a7fdd57a5627
“rayane” <r.germondchibane@myskolae.com>
2025-10-21 09:19:04 +0200

    création de l'interface graphique

---
+++ jeux/pong/pong.py
@@ -0,0 +1,8 @@
+import turtle
+
+# Configuration de la fenêtre
+fenetre = turtle.Screen()
+fenetre.title("Pong by Rayane")
+fenetre.bgcolor("black")
+fenetre.setup(width=800, height=600)
+fenetre.tracer(0)  # désactive les mises à jour automatiques
