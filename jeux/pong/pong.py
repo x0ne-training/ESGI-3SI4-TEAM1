e1bfb91c84a8f0251c76ea188f49592ba394c1f6
“rayane” <r.germondchibane@myskolae.com>
2025-10-21 09:23:17 +0200

    création de la raquette gauche

--- jeux/pong/pong.py
+++ jeux/pong/pong.py
@@ -10,3 +10,12 @@ fenetre.tracer(0)  # désactive les mises à jour automatiques
 # Score
 score_gauche = 0
 score_droite = 0
+
+# Raquette gauche
+raquette_gauche = turtle.Turtle()
+raquette_gauche.speed(0)
+raquette_gauche.shape("square")
+raquette_gauche.color("blue")
+raquette_gauche.shapesize(stretch_wid=5, stretch_len=1)
+raquette_gauche.penup()
+raquette_gauche.goto(-350, 0)
