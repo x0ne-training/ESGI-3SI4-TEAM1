41bc216a20badab622ecbe4dde656ce72b6eab69
lucie <lmanarin@myges.fr>
2025-10-21 08:10:08 +0200

    add: fonction du choix de l'ordinateur

--- jeux/pierre-feuille-ciseaux/fonctions.c
+++ jeux/pierre-feuille-ciseaux/fonctions.c
@@ -3,3 +3,13 @@
 #include <time.h>
 #include "pfc.h"
 
+int victoires_joueur = 0;
+int victoires_ordi = 0;
+int egalites = 0;
+
+int choixOrdinateur(){
+    int choix;
+    choix = (rand() % 3) + 1;
+    return choix;
+}
+
