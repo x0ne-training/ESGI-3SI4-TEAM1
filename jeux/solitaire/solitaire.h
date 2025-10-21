f78553f167a56125e12d3bed3e98cd4423138f76
lucie <lmanarin@myges.fr>
2025-10-21 08:58:45 +0200

    add: définition des fonctions

---
+++ jeux/solitaire/solitaire.h
@@ -0,0 +1,22 @@
+#ifndef SOLITAIRE_H
+#define SOLITAIRE_H
+
+#define ROUGE "\033[1;31m"
+#define NOIR "\033[1;30m"
+#define RESET "\033[0m"
+
+#define NB_CARTES_PYRAMIDE 28
+#define NB_CARTES_TOTAL 52
+
+#define CARTE_RETIREE -1
+#define CARTE_VALIDE 1
+#define CARTE_INVALIDE 0
+
+#define VICTOIRE 1
+#define PAS_VICTOIRE 0
+
+#define REJOUER 1
+#define NE_PAS_REJOUER 0
+
+
+#endif
