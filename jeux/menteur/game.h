fd58e7781d016b95581699e6670278a563ad0a58
Kevin Da Silva <k.dasilvaresende@myskolae.fr>
2025-10-22 14:51:40 +0200

    [ADD] Mini-jeu du Menteur complet sur branche Kevin

---
+++ jeux/menteur/game.h
@@ -0,0 +1,10 @@
+#ifndef GAME_H
+#define GAME_H
+
+void tour_joueur();
+void tour_ordi();
+int poser_carte(int joueur);
+int annoncer_carte(int joueur);
+int verifier_mentir(int vrai, int annonce);
+
+#endif
