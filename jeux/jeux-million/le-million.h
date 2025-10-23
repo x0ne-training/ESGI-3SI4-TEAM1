15ff7d11519da3bde59052c1efe0e66c2d2b7255
lucie <lmanarin@myges.fr>
2025-10-23 12:51:44 +0200

    fix: erreur de fonctions

--- jeux/jeux-million/le-million.h
+++ jeux/jeux-million/le-million.h
@@ -1,18 +1,31 @@
 #ifndef LE_MILLION_H
 #define LE_MILLION_H
 
-#define MAX_QUESTIONS 10
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_CHOICES 4
+#define MAX_QUESTIONS 15
 
 typedef struct {
-    char question[256];
-    char reponses[4][128];
-    char bonne_reponse;
-    int gain;
-} Question;
+    const char *question;
+    const char *choices[MAX_CHOICES];
+    int correct; // 0..3
 } Question;
 
-void afficher_question(Question q, int numero);
-char demander_reponse();
-int verifier_reponse(Question q, char rep);
+// accès aux questions
+void init_questions(void);
+int get_questions_count(void);
+const Question* get_question(int idx);
 
-#endif
+// jokers
+int joker_5050(int qidx, int out_choices[]); // remplit out_choices avec 2 indices valides (dont la bonne)
+void joker_phone(int qidx);
+void joker_audience(int qidx);
+
+// utilitaires
+int ask_user_choice(const Question *q, int removed_mask); // removed_mask: bits 0..3: si bit 1 => choix retiré
+void print_question(const Question *q, int removed_mask);
+
+#endif // LE_MILLION_H
