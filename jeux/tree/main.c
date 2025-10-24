7581832f00c227498791c061aea93f0e1f19cf43
Kevin Da Silva <k.dasilvaresende@myskolae.fr>
2025-10-24 10:34:24 +0200

    fix: accent

--- jeux/tree/main.c
+++ jeux/tree/main.c
@@ -2,15 +2,15 @@
 #include "fonctions.h"
 
 int main() {
-    // Création de l'arbre du jeu
-    Node* root = createNode("Voulez-vous aller à gauche ?");
+    // Creation de l'arbre du jeu
+    Node* root = createNode("Voulez-vous aller a gauche ?");
     root->left = createNode("Voulez-vous entrer dans la caverne ?");
     root->right = createNode("Voulez-vous traverser le pont ?");
 
-    root->left->left = createNode("Vous trouvez un trésor !");
-    root->left->right = createNode("Vous tombez dans un piège !");
-    root->right->left = createNode("Vous trouvez un trésor !");
-    root->right->right = createNode("Vous êtes perdu dans la forêt !");
+    root->left->left = createNode("Vous trouvez un tresor !");
+    root->left->right = createNode("Vous tombez dans un piege !");
+    root->right->left = createNode("Vous trouvez un tresor !");
+    root->right->right = createNode("Vous etes perdu dans la foret !");
 
     printf("Bienvenue dans le mini-jeu de l'arbre !\n");
     play(root);
