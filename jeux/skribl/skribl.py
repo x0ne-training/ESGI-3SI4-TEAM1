9cd1cc879aa2f5dfc53f7d7c904987ea4d334a56
“rayane” <r.germondchibane@myskolae.com>
2025-10-24 12:32:56 +0200

    feat: dessin basique avec la souris

--- jeux/skribl/skribl.py
+++ jeux/skribl/skribl.py
@@ -8,3 +8,10 @@ if __name__ == '__main__':
 
 canvas = tk.Canvas(root, bg='white', cursor='cross')
 canvas.pack(fill=tk.BOTH, expand=True)
+
+def draw(event):
+    x, y = event.x, event.y
+    r = 3
+    canvas.create_oval(x - r, y - r, x + r, y + r, fill='black', outline='black')
+
+canvas.bind('<B1-Motion>', draw)
