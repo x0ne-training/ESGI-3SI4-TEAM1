6c171525c6580340dc9debb315fdc740d70eed78
lucie <lmanarin@myges.fr>
2025-10-24 11:50:05 +0200

    add: definition des fonctions

---
+++ jeux/echec/echecs.h
@@ -0,0 +1,23 @@
+#ifndef ECHECS_H
+#define ECHECS_H
+
+#define TAILLE 8
+
+// Codes des pièces
+#define VIDE 0
+#define PION_B 1
+#define TOUR_B 2
+#define CAVALIER_B 3
+#define FOU_B 4
+#define DAME_B 5
+#define ROI_B 6
+#define PION_N 7
+#define TOUR_N 8
+#define CAVALIER_N 9
+#define FOU_N 10
+#define DAME_N 11
+#define ROI_N 12
+
+
+
+#endif
