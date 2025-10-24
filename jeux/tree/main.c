06e2fe8be2a22b38842344e0c716fd19d84c76eb
Kevin Da Silva <k.dasilvaresende@myskolae.fr>
2025-10-24 10:33:31 +0200

    add: Création de l'arbre du jeu

--- jeux/tree/main.c
+++ jeux/tree/main.c
@@ -6,15 +6,3 @@ int main() {
     Node* root = createNode("Voulez-vous aller à gauche ?");
     root->left = createNode("Voulez-vous entrer dans la caverne ?");
     root->right = createNode("Voulez-vous traverser le pont ?");
-
-    root->left->left = createNode("Vous trouvez un trésor !");
-    root->left->right = createNode("Vous tombez dans un piège !");
-    root->right->left = createNode("Vous trouvez un trésor !");
-    root->right->right = createNode("Vous êtes perdu dans la forêt !");
-
-    printf("Bienvenue dans le mini-jeu de l'arbre !\n");
-    play(root);
-
-    freeTree(root);
-    return 0;
-}
