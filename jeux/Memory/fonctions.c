8c344ac5791e4d979aaeeaab0efcb1ca6a287f6f
Kevin Da Silva <k.dasilvaresende@myskolae.fr>
2025-10-24 13:54:27 +0200

    add: victoire jeu

--- jeux/Memory/fonctions.c
+++ jeux/Memory/fonctions.c
@@ -1,25 +1,4 @@
 
-void playGame(Card board[SIZE][SIZE]) {
-    int x1, y1, x2, y2;
-
-    while(!isGameOver(board)) {
-        displayBoard(board);
-
-        printf("Choisissez la premiere carte (ligne colonne) : ");
-        scanf("%d %d", &x1, &y1);
-        printf("Choisissez la deuxieme carte (ligne colonne) : ");
-        scanf("%d %d", &x2, &y2);
-
-        if(x1 == x2 && y1 == y2) {
-            printf("Vous devez choisir deux cartes differentes!\n");
-            continue;
-        }
-
-        board[x1][y1].revealed = 1;
-        board[x2][y2].revealed = 1;
-
-        displayBoard(board);
-
         if(board[x1][y1].value == board[x2][y2].value) {
             printf("Bravo! C'est une paire!\n");
         } else {
