d496d81d9e694404b3bb185fd3e02f897230fd4b
Kevin Da Silva <k.dasilvaresende@myskolae.fr>
2025-10-24 14:02:06 +0200

    add: révélation du jeu

--- jeux/Memory/memory.h
+++ jeux/Memory/memory.h
@@ -1,4 +1,12 @@
-#ifndef MEMORY_H
-#define MEMORY_H
 
-#define SIZE 4 // 4x4 cartes
+typedef struct {
+    char value;
+    int revealed; // 0 = cachee, 1 = visible
+} Card;
+
+void initBoard(Card board[SIZE][SIZE]);
+void displayBoard(Card board[SIZE][SIZE]);
+int isGameOver(Card board[SIZE][SIZE]);
+void playGame(Card board[SIZE][SIZE]);
+
+#endif
