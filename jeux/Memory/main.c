19c6cff5ac66066bdf5665b6d41d4b2ef608b70c
Kevin Da Silva <k.dasilvaresende@myskolae.fr>
2025-10-24 14:01:16 +0200

    fix : implementation jeu

--- jeux/Memory/main.c
+++ jeux/Memory/main.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include "memory.h"
 
+int main() {
+    Card board[SIZE][SIZE];
+    initBoard(board);
+    printf("Bienvenue dans le jeu de Memoire !\n");
+    playGame(board);
+    return 0;
+}
