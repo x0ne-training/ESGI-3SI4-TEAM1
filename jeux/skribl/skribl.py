55cba2fd656d1e01a0dd9875796354a5b7517f07
“rayane” <r.germondchibane@myskolae.com>
2025-10-24 12:31:20 +0200

    feat: ajout du canvas principal

--- jeux/skribl/skribl.py
+++ jeux/skribl/skribl.py
@@ -5,3 +5,6 @@ if __name__ == '__main__':
     root.title("Mini Paint — Python/Tkinter")
     root.geometry('900x600')
     root.mainloop()
+
+canvas = tk.Canvas(root, bg='white', cursor='cross')
+canvas.pack(fill=tk.BOTH, expand=True)
