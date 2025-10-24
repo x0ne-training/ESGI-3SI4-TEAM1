5d94192c199bd488639680718d1e6dde2db65a95
Kevin Da Silva <k.dasilvaresende@myskolae.fr>
2025-10-24 13:55:44 +0200

    add: choix cartes

--- jeux/Memory/fonctions.c
+++ jeux/Memory/fonctions.c
@@ -1,14 +1,21 @@
 
-        if(board[x1][y1].value == board[x2][y2].value) {
-            printf("Bravo! C'est une paire!\n");
-        } else {
-            printf("Pas de chance.\n");
-            board[x1][y1].revealed = 0;
-            board[x2][y2].revealed = 0;
+void playGame(Card board[SIZE][SIZE]) {
+    int x1, y1, x2, y2;
+
+    while(!isGameOver(board)) {
+        displayBoard(board);
+
+        printf("Choisissez la premiere carte (ligne colonne) : ");
+        scanf("%d %d", &x1, &y1);
+        printf("Choisissez la deuxieme carte (ligne colonne) : ");
+        scanf("%d %d", &x2, &y2);
+
+        if(x1 == x2 && y1 == y2) {
+            printf("Vous devez choisir deux cartes differentes!\n");
+            continue;
         }
 
-        printf("\n");
-    }
+        board[x1][y1].revealed = 1;
+        board[x2][y2].revealed = 1;
 
-    printf("Felicitation! Vous avez trouve toutes les paires!\n");
-}
+        displayBoard(board);
