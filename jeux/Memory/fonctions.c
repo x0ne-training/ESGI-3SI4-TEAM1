f527d99e855655321fbc0784e6674c47a6371336
Kevin Da Silva <k.dasilvaresende@myskolae.fr>
2025-10-24 13:59:55 +0200

    add: introduction jeu

--- jeux/Memory/fonctions.c
+++ jeux/Memory/fonctions.c
@@ -19,3 +19,17 @@ void playGame(Card board[SIZE][SIZE]) {
         board[x2][y2].revealed = 1;
 
         displayBoard(board);
+
+        if(board[x1][y1].value == board[x2][y2].value) {
+            printf("Bravo! C'est une paire!\n");
+        } else {
+            printf("Pas de chance.\n");
+            board[x1][y1].revealed = 0;
+            board[x2][y2].revealed = 0;
+        }
+
+        printf("\n");
+    }
+
+    printf("Felicitation! Vous avez trouve toutes les paires!\n");
+}
--- jeux/Memory/main.c
+++ jeux/Memory/main.c
@@ -1,10 +1,1 @@
-#include <stdio.h>
-#include "memory.h"
 
-int main() {
-    Card board[SIZE][SIZE];
-    initBoard(board);
-    printf("Bienvenue dans le jeu de Memoire !\n");
-    playGame(board);
-    return 0;
-}
