49fbad291fd9eb63fb881a402989918e6c8d31c6
Kevin Da Silva <k.dasilvaresende@myskolae.fr>
2025-10-25 14:49:25 +0200

    fix changement du main

--- jeux/Memory/main.c
+++ jeux/Memory/main.c
@@ -3,8 +3,13 @@
 
 int main() {
     Card board[SIZE][SIZE];
+
+    printf("Bienvenue dans le jeu de memoire !\n");
+    printf("Trouvez toutes les paires de cartes identiques.\n\n");
+
     initBoard(board);
-    printf("Bienvenue dans le jeu de Memoire !\n");
     playGame(board);
+
     return 0;
 }
+
--- jeux/Memory/memory.h
+++ jeux/Memory/memory.h
@@ -1,13 +1,14 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
-#define SIZE 4 // 4x4 cartes
+#define SIZE 4 // taille du plateau (4x4 cartes)
 
 typedef struct {
     char value;
-    int revealed;
+    int revealed; // 0 = cachee, 1 = visible
 } Card;
 
+// Fonctions
 void initBoard(Card board[SIZE][SIZE]);
 void displayBoard(Card board[SIZE][SIZE]);
 int isGameOver(Card board[SIZE][SIZE]);
