b61b66f0eb504cfb760e2b975fd999146ab2e7d6
Kevin Da Silva <k.dasilvaresende@myskolae.fr>
2025-10-25 15:50:11 +0200

    Fin de tour

--- jeux/petits-chevaux/fonctions.c
+++ jeux/petits-chevaux/fonctions.c
@@ -1,25 +1,4 @@
 
-    if(pion->atHome) {
-        if(de == 6) {
-            pion->position = 1;
-            pion->atHome = 0;
-            printf("%s sort un pion de la maison !\n", joueur->nom);
-        } else {
-            printf("%s ne peut pas sortir le pion (besoin de 6).\n", joueur->nom);
-        }
-        return;
-    }
-
-    pion->position += de;
-    if(pion->position >= CASES_TOTAL) {
-        pion->position = CASES_TOTAL;
-        pion->finished = 1;
-        printf("%s a amene un pion a la fin !\n", joueur->nom);
-    } else {
-        printf("%s deplace un pion de %d cases.\n", joueur->nom, de);
-    }
-}
-
 int isGameOver(Joueur joueurs[NB_JOUEURS]) {
     for(int i = 0; i < NB_JOUEURS; i++) {
         for(int j = 0; j < PIONS_PAR_JOUEUR; j++) {
