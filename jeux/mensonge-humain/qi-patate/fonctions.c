e050eafb0d0e12f13202cafc9accd311c03b8b9a
lucie <lmanarin@myges.fr>
2025-10-26 23:34:21 +0100

    add: analyse des questions

--- jeux/mensonge-humain/qi-patate/fonctions.c
+++ jeux/mensonge-humain/qi-patate/fonctions.c
@@ -3,3 +3,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include "qi-patate.h"
+
+void intro() {
+    system("cls");
+    printf("Bienvenue dans le JEU DU QI DE PATATE\n\n");
+    printf("Reponds aux questions et je calculerai ton QI de patate.\n\n");
+}
+
+void poser_question(const char *question, char *reponse, int taille) {
+    printf("%s ", question);
+    fgets(reponse, taille, stdin);
+    reponse[strcspn(reponse, "\n")] = 0;
+}
