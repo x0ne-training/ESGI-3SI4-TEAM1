f44bd1095512b4dada40ea7579c0d8f13cc28237
lucie <lmanarin@myges.fr>
2025-10-26 23:36:55 +0100

    fix: déclaration

--- jeux/mensonge-humain/qi-patate/main.c
+++ jeux/mensonge-humain/qi-patate/main.c
@@ -65,3 +65,10 @@ int main() {
 
     poser_question("20. Quelle est la meilleure maniere de cuisiner une patate ?", reponse, 100);
     analyse(reponse);
+
+    verdict_final();
+
+    printf("\nAppuie sur une touche pour quitter...\n");
+    system("pause");
+    return 0;
+}
