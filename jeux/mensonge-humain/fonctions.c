6533f21782139224f70b4f035d9812f20897bd4e
lucie <lmanarin@myges.fr>
2025-10-26 23:00:50 +0100

    refactor: analyse questions

--- jeux/mensonge-humain/fonctions.c
+++ jeux/mensonge-humain/fonctions.c
@@ -11,3 +11,23 @@ void intro() {
     printf("Je vais détecter si tu mens grâce à mes... 'algorithmes secrets'.\n\n");
 }
 
+void poser_question(const char *question, char *reponse, int taille) {
+    printf("%s ", question);
+    fgets(reponse, taille, stdin);
+    reponse[strcspn(reponse, "\n")] = 0;
+}
+
+void analyse(const char *reponse) {
+    srand((unsigned)time(NULL) + rand());
+    int chance = rand() % 3; // 0, 1 ou 2
+    if (strlen(reponse) == 0) {
+        printf("Tu ne dis rien ? CLASSIQUE du menteur professionnel 😏\n");
+    } else if (chance == 0) {
+        printf("Je te crois... (mais je te surveille 👀)\n");
+    } else if (chance == 1) {
+        printf("Mouais... Ça sent le mensonge à plein nez ! 😤\n");
+    } else {
+        printf("HAHA tu crois me tromper ? C’est clairement un bobard 😂\n");
+    }
+}
+
