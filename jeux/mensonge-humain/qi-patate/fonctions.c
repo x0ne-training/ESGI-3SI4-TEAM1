ffb1a811a928ec33a5c4c204404683ec8ca9675f
lucie <lmanarin@myges.fr>
2025-10-26 23:34:30 +0100

    add: analyse finale

--- jeux/mensonge-humain/qi-patate/fonctions.c
+++ jeux/mensonge-humain/qi-patate/fonctions.c
@@ -15,3 +15,33 @@ void poser_question(const char *question, char *reponse, int taille) {
     fgets(reponse, taille, stdin);
     reponse[strcspn(reponse, "\n")] = 0;
 }
+
+void analyse(const char *reponse) {
+    srand((unsigned)time(NULL) + rand());
+    int r = rand() % 3;
+    if (strlen(reponse) == 0) {
+        printf("Tu n'as rien repondu, patate silencieuse.\n\n");
+    } else if (r == 0) {
+        printf("Interressant choix.\n\n");
+    } else if (r == 1) {
+        printf("Hum, patate douteuse...\n\n");
+    } else {
+        printf("Ceci va influencer ton QI de patate.\n\n");
+    }
+}
+
+void verdict_final() {
+    srand((unsigned)time(NULL));
+    int qi = rand() % 101; // 0 a 100
+    printf("Ton QI de patate est de %d.\n", qi);
+    if (qi < 20) {
+        printf("Incroyable ! Meme une patate de jardin est plus intelligente.\n");
+    } else if (qi < 50) {
+        printf("Tu es une patate moyenne, rien de special.\n");
+    } else if (qi < 80) {
+        printf("Bravo ! Patate bien avancee.\n");
+    } else {
+        printf("Exceptionnel ! Patate de genie.\n");
+    }
+    printf("\nMerci d'avoir joue au QI de patate !\n");
+}
