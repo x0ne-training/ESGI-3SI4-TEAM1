88cef4592f3383db79eb800736c90842fe842e75
lucie <lmanarin@myges.fr>
2025-10-26 22:59:22 +0100

    fix: en-tête

--- jeux/mensonge-humain/main.c
+++ jeux/mensonge-humain/main.c
@@ -6,3 +6,18 @@ int main() {
     char reponse[100];
     intro();
 
+    poser_question("As-tu déjà menti à ta mère ?", reponse, 100);
+    analyse(reponse);
+
+    poser_question("\nAs-tu déjà mangé quelque chose tombé par terre ?", reponse, 100);
+    analyse(reponse);
+
+    poser_question("\nPenses-tu être plus intelligent qu’une IA ?", reponse, 100);
+    analyse(reponse);
+
+    verdict();
+
+    printf("\nAppuie sur une touche pour quitter...\n");
+    system("pause");
+    return 0;
+}
