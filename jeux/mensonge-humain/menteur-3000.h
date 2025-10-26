83d357f43bfa510fdd5bd503b63f5c39d84094c0
lucie <lmanarin@myges.fr>
2025-10-26 23:02:56 +0100

    add: initialisation

--- jeux/mensonge-humain/menteur-3000.h
+++ jeux/mensonge-humain/menteur-3000.h
@@ -1,2 +1,9 @@
 #ifndef MENTEUR_3000_H
 #define MENTEUR_3000_H
+
+void intro();
+void poser_question(const char *question, char *reponse, int taille);
+void analyse(const char *reponse);
+void verdict();
+
+#endif
