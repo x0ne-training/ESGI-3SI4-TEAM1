38623e61fb5a68c8d6d4b328c30fb14f8eb17db4
lucie <lmanarin@myges.fr>
2025-10-26 23:00:11 +0100

    add: introduction

--- jeux/mensonge-humain/fonctions.c
+++ jeux/mensonge-humain/fonctions.c
@@ -3,3 +3,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include "menteur-3000.h"
+
+void intro() {
+    system("cls");
+    printf("🤥 Bienvenue dans le JEU DU MENTEUR 3000 🤥\n\n");
+    printf("Je suis une IA ultra sophistiquée (selon moi).\n");
+    printf("Je vais détecter si tu mens grâce à mes... 'algorithmes secrets'.\n\n");
+}
+
