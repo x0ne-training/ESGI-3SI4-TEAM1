5a29a4673217f03b98b65fb4036c6257375fbcf6
Kevin Da Silva <k.dasilvaresende@myskolae.fr>
2025-10-27 17:14:25 +0100

    [fix: MAJ de la structure]

--- jeux/undercover/undercover.h
+++ jeux/undercover/undercover.h
@@ -6,18 +6,26 @@
 
 typedef enum { CITOYEN, UNDERCOVER, MR_WHITE } Role;
 
+#ifndef UNDERCOVER_H
+#define UNDERCOVER_H
+
+#define MAX_JOUEURS 20
+#define MAX_MOT 50
+
+typedef enum {CITOYEN, UNDERCOVER, MR_WHITE} Role;
+
 typedef struct {
     char nom[20];
+    char mot[MAX_MOT];
     Role role;
-    char mot[MAX_MOT];   // mot secret
-    int actif;           // 1 = encore en jeu, 0 = éliminé
+    int actif; // 1 = en jeu, 0 = éliminé
 } Joueur;
 
-// Fonctions du jeu
+// Fonctions principales
 void initGame(Joueur joueurs[MAX_JOUEURS], int nbJoueurs);
-void distribuerMots(Joueur joueurs[MAX_JOUEURS], int nbJoueurs, const char* motCitoyen, const char* motUndercover, int hasMrWhite);
-void afficherJoueursActifs(Joueur joueurs[MAX_JOUEURS], int nbJoueurs);
+void distribuerMots(Joueur joueurs[MAX_JOUEURS], int nbJoueurs, char* motCitoyen, char* motUndercover);
+void afficherJoueurs(Joueur joueurs[MAX_JOUEURS], int nbJoueurs);
 int vote(Joueur joueurs[MAX_JOUEURS], int nbJoueurs);
-int checkVictory(Joueur joueurs[MAX_JOUEURS], int nbJoueurs, const char* motCitoyen);
+int checkVictory(Joueur joueurs[MAX_JOUEURS], int nbJoueurs);
 
 #endif
