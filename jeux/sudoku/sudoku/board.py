33350689de3548ca97b9fa6299c23d194b64a50f
yasminetrki <yasminetriki015@gmail.com>
2025-10-29 21:35:53 +0100

    add: board

---
+++ jeux/sudoku/sudoku/board.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+from typing import List, Tuple, Set
+
+Grid = List[List[int]]
+
+class Board:
+    """
+    Grille Sudoku 9x9.
+    - 0 = case vide
+    """
+    
