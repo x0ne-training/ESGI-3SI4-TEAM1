37daf36ad336d7fdd2a5521357dea91f56510fbb
yasminetrki <yasminetriki015@gmail.com>
2025-10-29 22:11:52 +0100

    add: affiche grille

--- jeux/sudoku/sudoku/cli.py
+++ jeux/sudoku/sudoku/cli.py
@@ -22,3 +22,21 @@ Commandes:
   q         -> quitter
 """
 
+def _print_board(board: Board):
+    lines = []
+    for r, row in enumerate(board.grid):
+        if r % 3 == 0:
+            lines.append("+-------+-------+-------+")
+        line = []
+        for c, v in enumerate(row):
+            if c % 3 == 0:
+                line.append("| ")
+            line.append("." if v == 0 else str(v))
+            line.append(" ")
+        line.append("|")
+        lines.append("".join(line))
+    lines.append("+-------+-------+-------+")
+    col_idx = "    " + " ".join(str(i) for i in range(1, 10))
+    print("\n".join(lines))
+    print(col_idx)
+
