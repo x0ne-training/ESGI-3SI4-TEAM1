0337069003ec2850a5ca53c2b15a3c705bddfa20
yasminetrki <yasminetriki015@gmail.com>
2025-10-29 21:51:52 +0100

    add: constructeur _init_ dans board

--- jeux/sudoku/sudoku/board.py
+++ jeux/sudoku/sudoku/board.py
@@ -8,4 +8,10 @@ class Board:
     Grille Sudoku 9x9.
     - 0 = case vide
     """
+
+    def __init__(self, grid: Grid):
+        if len(grid) != 9 or any(len(row) != 9 for row in grid):
+            raise ValueError("La grille doit être 9x9.")
+        self.grid: Grid = [row[:] for row in grid]
+
     
