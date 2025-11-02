4b581d17c9795c62a501f99aae9f35c5d47f7c5b
luciemanarin <lmanarin@myges.fr>
2025-11-02 16:27:56 +0100

    add: application des choix

--- jeux/football/game.c
+++ jeux/football/game.c
@@ -16,3 +16,37 @@ int get_player_choice() {
     scanf("%d", &choice);
     return choice;
 }
+
+void apply_choice(int choice, Player *player, Player *adversaire, int *score_player, int *score_adv) {
+    switch(choice) {
+        case 1:
+            if(action_success(player->tir)) {
+                printf("But pour vous !\n");
+                (*score_player)++;
+            } else {
+                printf("Tir manqué.\n");
+            }
+            break;
+        case 2:
+            printf(action_success(player->passe) ? "Passe réussie.\n" : "Passe échouée.\n");
+            break;
+        case 3:
+            printf(action_success(player->dribble) ? "Dribble réussi.\n" : "Perte de balle.\n");
+            break;
+        case 4:
+            printf("Défense...\n");
+            if(action_success(adversaire->tir)) {
+                printf("L'adversaire marque pendant votre défense !\n");
+                (*score_adv)++;
+            }
+            break;
+        default:
+            printf("Choix invalide.\n");
+    }
+
+    // Tour aléatoire adversaire
+    if(rand() % 2 == 0 && action_success(adversaire->tir)) {
+        printf("L'adversaire tire... et marque !\n");
+        (*score_adv)++;
+    }
+}
