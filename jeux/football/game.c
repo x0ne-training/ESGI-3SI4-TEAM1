2e6fd8b393b73b26de5964ad8b65fed77b96f659
luciemanarin <lmanarin@myges.fr>
2025-11-02 16:26:01 +0100

    add: affichage des statistiques

--- jeux/football/game.c
+++ jeux/football/game.c
@@ -5,3 +5,7 @@
 int action_success(int skill) {
     return rand() % 100 < skill;
 }
+
+void print_stats(Player p) {
+    printf("Stats - Tir:%d Passe:%d Dribble:%d Endurance:%d\n", p.tir, p.passe, p.dribble, p.endurance);
+}
