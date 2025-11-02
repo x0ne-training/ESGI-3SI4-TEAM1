8bd1e807e26790c04b83b82f516a8ff0f3779299
luciemanarin <lmanarin@myges.fr>
2025-11-02 16:24:58 +0100

    add: initialisation

--- jeux/football/game.h
+++ jeux/football/game.h
@@ -8,5 +8,9 @@ typedef struct {
     int endurance;
 } Player;
 
+int action_success(int skill);
+void print_stats(Player p);
+int get_player_choice();
+void apply_choice(int choice, Player *player, Player *adversaire, int *score_player, int *score_adv);
 
 #endif
