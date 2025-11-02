fb48e66e9c7bfad8fb84e7480493e9172477ef34
luciemanarin <lmanarin@myges.fr>
2025-11-02 17:04:31 +0100

    add: logique du jeu

--- jeux/loup-garou/main.c
+++ jeux/loup-garou/main.c
@@ -3,3 +3,22 @@
 #include <time.h>
 #include "game.h"
 
+int main() {
+    srand(time(NULL));
+    int numPlayers;
+    printf("Nombre de joueurs: ");
+    scanf("%d", &numPlayers);
+    Player players[numPlayers];
+
+    initPlayers(players, numPlayers);
+
+    while(!gameOver(players, numPlayers)) {
+        nightPhase(players, numPlayers);
+        dayPhase(players, numPlayers);
+        printStatus(players, numPlayers);
+    }
+
+    declareWinner(players, numPlayers);
+
+    return 0;
+}
