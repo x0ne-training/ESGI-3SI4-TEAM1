5e046733dc32ec8b81057d44032bcf51aec73da0
luciemanarin <lmanarin@myges.fr>
2025-11-02 21:56:55 +0100

    add: logique du jeu

--- jeux/chasse_tresor/main.c
+++ jeux/chasse_tresor/main.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
 #include "game.h"
 
+int main() {
+    Player player;
+    Cell grid[GRID_SIZE][GRID_SIZE];
 
+    initializeGame(&player, grid);
+
+    while (!isGameOver(&player, grid)) {
+        displayGrid(&player, grid);
+        printf("\nDéplacez-vous : 1=Haut, 2=Bas, 3=Gauche, 4=Droite : ");
+        int choice;
+        scanf("%d", &choice);
+        movePlayer(&player, grid, choice);
+    }
+
+    endGame(&player);
+    return 0;
+}
