f0219f79e163b1981cdb45769ab89aa64b361b97
luciemanarin <lmanarin@myges.fr>
2025-11-02 18:41:50 +0100

    add: logique du jeu

--- jeux/labyrinth/main.c
+++ jeux/labyrinth/main.c
@@ -2,3 +2,22 @@
 #include <stdlib.h>
 #include "game.h"
 
+int main() {
+    Player player;
+    initializePlayer(&player);
+
+    Maze maze;
+    generateMaze(&maze, 5, 5); // 5x5 labyrinth
+
+    printf("Bienvenue dans le Labyrinthe ! Trouvez la sortie.\n");
+
+    while (!isGameOver(&player, &maze)) {
+        displayMaze(&maze, &player);
+        playTurn(&player, &maze);
+    }
+
+    endGame(&player, &maze);
+
+    freeMaze(&maze);
+    return 0;
+}
