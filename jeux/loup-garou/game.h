0a948902ad1ffa09fc643055da66ec5979df7cc4
luciemanarin <lmanarin@myges.fr>
2025-11-02 17:02:05 +0100

    add: fonctions

--- jeux/loup-garou/game.h
+++ jeux/loup-garou/game.h
@@ -3,5 +3,11 @@
 
 #include "player.h"
 
+void initPlayers(Player players[], int numPlayers);
+void printStatus(Player players[], int numPlayers);
+void nightPhase(Player players[], int numPlayers);
+void dayPhase(Player players[], int numPlayers);
+int gameOver(Player players[], int numPlayers);
+void declareWinner(Player players[], int numPlayers);
 
 #endif
