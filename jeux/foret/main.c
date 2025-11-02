79b245c0a00ba19988a0beeda405a308c4e46446
luciemanarin <lmanarin@myges.fr>
2025-11-02 18:07:35 +0100

    add: logique du jeu

--- jeux/foret/main.c
+++ jeux/foret/main.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
 #include "game.h"
 
+int main(void) {
+    printf("Bienvenue dans *La Forêt Maudite* 🌲\n");
+    printf("Votre objectif : survivre le plus longtemps possible.\n");
 
+    Player player;
+    initializePlayer(&player);
+
+    while (!isGameOver(&player)) {
+        displayStats(&player);
+        playTurn(&player);
+    }
+
+    endGame(&player);
+    return 0;
+}
