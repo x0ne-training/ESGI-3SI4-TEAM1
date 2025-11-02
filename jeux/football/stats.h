a42beb3742a01496a536ffc57e1a64afd8cfccea
luciemanarin <lmanarin@myges.fr>
2025-11-02 16:30:36 +0100

    add: structures

--- jeux/football/stats.h
+++ jeux/football/stats.h
@@ -1,6 +1,11 @@
 #ifndef STATS_H
 #define STATS_H
 
-
+typedef struct {
+    int energy;     // Énergie du joueur
+    int morale;     // Morale de l'équipe
+    int goals;      // Buts marqués
+    int fouls;      // Nombre de fautes
+} Stats;
 
 #endif // STATS_H
