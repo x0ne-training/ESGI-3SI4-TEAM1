2a1283b6b6e438582f0c60f50a658b793befc3eb
luciemanarin <lmanarin@myges.fr>
2025-11-02 16:33:57 +0100

    refactor: mep logique du jeu

--- jeux/football/main.c
+++ jeux/football/main.c
@@ -2,25 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include "game.h"
+#include "questions.h"
+#include "stats.h"
 
 int main() {
-    srand(time(NULL));
-    Player player = {70, 80, 60, 100};
-    Player adversaire = {65, 75, 55, 100};
+    srand(time(NULL)); // Pour l'aléatoire
 
-    int score_player = 0, score_adv = 0;
-    int minute;
+    Stats player = {100, 50, 0, 0};
+    Stats opponent = {100, 50, 0, 0};
 
-    printf("Bienvenue dans CLI-FIFA 25 !\n");
+    printf("Bienvenue dans Mini FIFA CLI !\n");
+    playMatch(&player, &opponent);
 
-    for(minute = 1; minute <= 5; minute++) {  // match court pour test
-        printf("\n--- Minute %d ---\n", minute);
-        print_stats(player);
-
-        int choice = get_player_choice();
-        apply_choice(choice, &player, &adversaire, &score_player, &score_adv);
-    }
-
-    printf("\nMatch terminé ! Score final : Vous %d - Adversaire %d\n", score_player, score_adv);
     return 0;
 }
