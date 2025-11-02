f5cb5fe1c074b55f7f68c7d99541b80e7af82673
luciemanarin <lmanarin@myges.fr>
2025-11-02 16:06:05 +0100

    fix: fonctions inutilisées

--- jeux/reigns/game.c
+++ jeux/reigns/game.c
@@ -14,6 +14,7 @@ void printStats(Stats stats) {
 }
 
 void applyChoice(int choice, Stats *stats, int effects[4]) {
+    (void)choice;
     stats->power  += effects[0];
     stats->money  += effects[1];
     stats->faith  += effects[2];
