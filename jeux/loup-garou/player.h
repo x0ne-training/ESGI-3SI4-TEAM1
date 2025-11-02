dc4d5b6ab10d2cf05bdff81fe3e877ade3d2bd6f
luciemanarin <lmanarin@myges.fr>
2025-11-02 16:58:56 +0100

    add: structure

--- jeux/loup-garou/player.h
+++ jeux/loup-garou/player.h
@@ -1,6 +1,12 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+typedef enum { VILLAGEOIS, LOUP, VOYANTE, CHASSEUR } Role;
 
+typedef struct {
+    char name[50];
+    Role role;
+    int alive; // 1 = vivant, 0 = mort
+} Player;
 
 #endif
