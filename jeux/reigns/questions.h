75c55f51e7c3268c54d75dc08f53181376a9928b
luciemanarin <lmanarin@myges.fr>
2025-11-02 15:57:38 +0100

    fix: structures

--- jeux/reigns/questions.h
+++ jeux/reigns/questions.h
@@ -3,5 +3,13 @@
 
 #include "game.h"
 
+typedef struct {
+    char question[256];
+    char optionA[128];
+    char optionB[128];
+    int effectsA[4];
+    int effectsB[4];
+} Question;
+
 
 #endif
