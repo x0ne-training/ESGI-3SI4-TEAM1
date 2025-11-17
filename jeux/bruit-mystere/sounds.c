2f59b871a9d2558ba35666305e67c227eb73d92d
lucie <lmanarin@myges.fr>
2025-11-17 16:15:05 +0100

    add: affichage message erreur

--- jeux/bruit-mystere/sounds.c
+++ jeux/bruit-mystere/sounds.c
@@ -8,3 +8,16 @@ int ask_sound(const char *sound, const char *expected) {
     printf("👉 Quel est ce son ? : \"%s\"\n> ", sound);
     scanf("%49s", answer);
 
+    // Convertir en minuscules
+    for (int i = 0; answer[i]; i++)
+        if (answer[i] >= 'A' && answer[i] <= 'Z')
+            answer[i] += 32;
+
+    if (strcmp(answer, expected) == 0) {
+        printf("✔ Correct !\n\n");
+        return 1;
+    }
+
+    printf("Faux ! C'était : %s\n\n", expected);
+    return 0;
+}
