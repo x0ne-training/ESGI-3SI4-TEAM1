34a86b3ec69ae038283fc106b81d39d77045a95e
lucie <lmanarin@myges.fr>
2025-11-17 10:20:33 +0100

    refactor: affichage final

--- jeux/mensonge-ok/main.c
+++ jeux/mensonge-ok/main.c
@@ -2,6 +2,12 @@
 #include "functions.h"
 
 int main(void) {
-    printf("=== JEU DU MENSONGE ===\n");
-    printf(" Règle : réponds OBLIGATOIREMENT le contraire de la vérité.\n\n");
+    printf("===JEU DU MENSONGE ===\n");
+    printf("Règle : réponds OBLIGATOIREMENT le contraire de la vérité.\n\n");
+
+    if (!ask("Est-ce que 2+2=4 ?", 1)) return 0;
+    if (!ask("Est-ce que les oiseaux sont des poissons ?", 0)) return 0;
+    if (!ask("Est-ce que tu es humain ?", 1)) return 0;
+    if (!ask("Est-ce qu'une pizza est liquide ?", 0)) return 0;
+    if (!ask("Est-ce que tu mens en ce moment ?", 1)) return 0;
 
