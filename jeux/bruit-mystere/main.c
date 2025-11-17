11570a230adcaccd6c39d61895c71a887bf7d483
lucie <lmanarin@myges.fr>
2025-11-17 16:11:05 +0100

    add: messages

--- jeux/bruit-mystere/main.c
+++ jeux/bruit-mystere/main.c
@@ -2,3 +2,10 @@
 #include <stdlib.h>
 #include "sounds.h"
 
+int main() {
+    int score = 0;
+    printf("=== 🔊 JEU DU BRUIT MYSTERE ===\n");
+    printf("Devine à quoi correspond le bruit !\n");
+    printf("(Réponds avec un mot : ex: chat, voiture, explosion)\n\n");
+
+    
