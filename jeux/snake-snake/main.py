8e631c1d4759b1a4e2832a2db63b182490e20f55
aureTheDev <aure.the.dev@pm.me>
2025-11-17 15:16:06 +0100

    feat: Définit la grille de jeu et les constantes

--- jeux/snake-snake/main.py
+++ jeux/snake-snake/main.py
@@ -1,4 +1,11 @@
 # main.py
 # Projet pour créer le jeu Snake en mode texte.
 
+# --- Constantes ---
+LARGEUR = 20
+HAUTEUR = 10
+
+# --- État du jeu ---
+# (vide pour l'instant)
+
 print("Lancement du jeu Snake...")
