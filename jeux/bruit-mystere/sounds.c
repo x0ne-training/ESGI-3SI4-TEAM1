90630c11a60c287ef7bc0575a539d4e34b3fea72
lucie <lmanarin@myges.fr>
2025-11-17 16:12:46 +0100

    add: définitions

--- jeux/bruit-mystere/sounds.c
+++ jeux/bruit-mystere/sounds.c
@@ -2,4 +2,9 @@
 #include <string.h>
 #include "sounds.h"
 
+int ask_sound(const char *sound, const char *expected) {
+    char answer[50];
+
+    printf("👉 Quel est ce son ? : \"%s\"\n> ", sound);
+    scanf("%49s", answer);
 
