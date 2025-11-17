40419dbc4d621dd85f81517d004339ca59b6f8d5
aureTheDev <aure.the.dev@pm.me>
2025-11-17 14:58:54 +0100

    feat: Ajoute une boucle de jeu principale infinie

--- jeux/py-adventure/main.py
+++ jeux/py-adventure/main.py
@@ -1,3 +1,8 @@
 # main.py
 # Début de notre jeu d'aventure textuelle
-print("Le jeu se lance...")
+
+# Boucle principale du jeu
+while True:
+    pass # "pass" est une instruction qui ne fait rien, en attendant de la remplir
+
+print("Merci d'avoir joué !")
