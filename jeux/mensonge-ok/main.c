650ece80c42d0c48b9e009ec2bf97fdd31be268a
lucie <lmanarin@myges.fr>
2025-11-17 11:02:54 +0100

    fix: accents

--- jeux/mensonge-ok/main.c
+++ jeux/mensonge-ok/main.c
@@ -2,8 +2,8 @@
 #include "functions.h"
 
 int main(void) {
-    printf("===JEU DU MENSONGE ===\n");
-    printf("Règle : réponds OBLIGATOIREMENT le contraire de la vérité.\n\n");
+    printf("=== JEU DU MENSONGE ===\n");
+    printf("Regle : reponds OBLIGATOIREMENT le contraire de la verite.\n\n");
 
     if (!ask("Est-ce que 2+2=4 ?", 1)) return 0;
     if (!ask("Est-ce que les oiseaux sont des poissons ?", 0)) return 0;
@@ -11,7 +11,7 @@ int main(void) {
     if (!ask("Est-ce qu'une pizza est liquide ?", 0)) return 0;
     if (!ask("Est-ce que tu mens en ce moment ?", 1)) return 0;
 
-    printf("\nTU AS GAGNÉ !!!\n");
-    printf(" Tu es officiellement un menteur professionnel.\n");
+    printf("\nTU AS GAGNE !!!\n");
+    printf("Tu es officiellement un menteur professionnel.\n");
     return 0;
 }
