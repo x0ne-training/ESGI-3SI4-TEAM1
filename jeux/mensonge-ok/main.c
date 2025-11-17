22c9fb83d77080add4f0441d6847e6d453feabad
lucie <lmanarin@myges.fr>
2025-11-17 10:20:08 +0100

    fix: affichage

--- jeux/mensonge-ok/main.c
+++ jeux/mensonge-ok/main.c
@@ -2,6 +2,6 @@
 #include "functions.h"
 
 int main(void) {
-    printf("=== 🧠 JEU DU MENSONGE ===\n");
-    printf("⚠️  Règle : réponds OBLIGATOIREMENT le contraire de la vérité.\n\n");
+    printf("=== JEU DU MENSONGE ===\n");
+    printf(" Règle : réponds OBLIGATOIREMENT le contraire de la vérité.\n\n");
 
