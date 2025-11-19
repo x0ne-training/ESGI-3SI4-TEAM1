1bfe2dd8625d5e3ba4f091010315a743c7c8f6e7
lucie <lmanarin@myges.fr>
2025-11-19 14:02:01 +0100

    add: structure du jeu

--- jeux/sims/sims.h
+++ jeux/sims/sims.h
@@ -13,3 +13,27 @@ typedef struct {
     int day;
     int alive;
 } SimsState;
+
+typedef struct {
+    SimsState sims[MAX_SIMS];
+    int count;
+    int current;
+} Game;
+
+void init_game(Game *g);
+int create_sim(Game *g, const char *name);
+void show_all_sims(const Game *g);
+void switch_sim(Game *g, int index);
+
+void eat(SimsState *s);
+void sleep_sims(SimsState *s);
+void work(SimsState *s);
+void fun(SimsState *s);
+void next_day(SimsState *s);
+void show_stats(const SimsState *s);
+int is_dead(const SimsState *s);
+
+int read_int_range(int min, int max);
+void clear_input(void);
+
+#endif
