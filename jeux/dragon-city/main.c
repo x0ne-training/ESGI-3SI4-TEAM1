b3e4379814d22b4c9dc8dce29a81a3fd5b76162d
lucie <lmanarin@myges.fr>
2025-11-19 11:31:07 +0100

    add: affichage du menu

---
+++ jeux/dragon-city/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "functions.h"
+
+void print_menu(void) {
+    printf("\n--- Dragon Mini (console) ---\n");
+    printf("1) Lister mes dragons\n");
+    printf("2) Élever un nouveau dragon\n");
+    printf("3) Nourrir un dragon (5 or)\n");
+    printf("4) Entraîner un dragon (10 or)\n");
+    printf("5) Faire combattre un dragon contre un monstre\n");
+    printf("6) Passer au jour suivant\n");
+    printf("7) Afficher infos du jeu\n");
+    printf("0) Quitter\n");
+    printf("Choix : ");
+}
