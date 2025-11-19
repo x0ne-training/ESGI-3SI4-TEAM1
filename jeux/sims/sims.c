302431e2295797ab5420eeb4f63cf29025bd198a
lucie <lmanarin@myges.fr>
2025-11-19 14:03:05 +0100

    add: initialisation sims

--- jeux/sims/sims.c
+++ jeux/sims/sims.c
@@ -8,3 +8,15 @@ static int clamp(int v, int a, int b) {
     if (v > b) return b;
     return v;
 }
+
+void init_sims(SimsState *s, const char *name) {
+    strncpy(s->name, name, MAX_NAME - 1);
+    s->name[MAX_NAME - 1] = '\0';
+    s->hunger = 70;
+    s->energy = 70;
+    s->mood = 70;
+    s->money = 100;
+    s->day = 1;
+    s->alive = 1;
+}
+
