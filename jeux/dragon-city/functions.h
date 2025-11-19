2e64757317d72549e4c13f4d99af6054802b29a6
lucie <lmanarin@myges.fr>
2025-11-19 11:27:13 +0100

    add: structure dragon

--- jeux/dragon-city/functions.h
+++ jeux/dragon-city/functions.h
@@ -6,3 +6,12 @@
 #define MAX_DRAGONS 10
 #define NAME_LEN 32
 
+typedef struct {
+    char name[NAME_LEN];
+    int level;
+    int health;      // max 100
+    int hunger;      // 0 (faim zéro) .. 100 (faim max => malus)
+    int attack;
+    bool alive;
+} Dragon;
+
