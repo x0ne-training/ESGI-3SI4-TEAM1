bffd8a99f809c1b8d8c441f46fda35f3901a7af2
lucie <lmanarin@myges.fr>
2025-11-19 14:01:52 +0100

    add: structure de mode

--- jeux/sims/sims.h
+++ jeux/sims/sims.h
@@ -3,3 +3,13 @@
 
 #define MAX_NAME 32
 #define MAX_SIMS 10
+
+typedef struct {
+    char name[MAX_NAME];
+    int hunger;
+    int energy;
+    int mood;
+    int money;
+    int day;
+    int alive;
+} SimsState;
