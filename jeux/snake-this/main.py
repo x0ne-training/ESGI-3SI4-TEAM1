2e1d9c713f0d75f9af5ab0b6c4e0b3c79ecdd5fd
aureTheDev <aure.the.dev@pm.me>
2025-11-20 08:59:56 +0100

    Implémentation du mouvement de base du serpent

--- jeux/snake-this/main.py
+++ jeux/snake-this/main.py
@@ -1,4 +1,5 @@
 import turtle
+import time
 
 # Configuration de la fenêtre
 wn = turtle.Screen()
@@ -16,6 +17,26 @@ head.penup()
 head.goto(0,0)
 head.direction = "stop"
 
+# Fonction de mouvement
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
 # Boucle principale du jeu
 while True:
     wn.update()
+    move()
+    time.sleep(0.1)
