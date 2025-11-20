726e3ec1bf631f7aef209be30ded240ca3ce04ec
aureTheDev <aure.the.dev@pm.me>
2025-11-20 08:59:45 +0100

    Création de la tête du serpent

--- jeux/snake-this/main.py
+++ jeux/snake-this/main.py
@@ -5,7 +5,16 @@ wn = turtle.Screen()
 wn.title("Jeu Snake")
 wn.bgcolor("black")
 wn.setup(width=600, height=600)
-wn.tracer(0) # Désactive les mises à jour de l'écran
+wn.tracer(0)
+
+# Tête du serpent
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("green")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
 
 # Boucle principale du jeu
 while True:
